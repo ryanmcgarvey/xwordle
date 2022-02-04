a4369d50d1b2d1374d23b5e66e61aa74d0c39e46
Ryan McGarvey <mcgarvey.ryan@gmail.com>
2022-02-04 12:03:05 -0500

    Randomize answers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,8 @@ class GamesController < ApplicationController
   def index
     @count = session[:count].to_i
   end
+
+  def new
+    @word = File.read(File.join(Rails.root, 'answers.txt')).split.sample
+  end
 end
